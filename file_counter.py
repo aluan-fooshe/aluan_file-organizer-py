77078a62e2488da88de1080b335757c61ce35927
aluan_fooshe <audreyluan8899@gmail.com>
2025-09-13 15:20:53 -0700

    added file count for JSON files in file_counter.py and subfile_counter.py for sorting Google Takeout photos and videos.

--- file_counter.py
+++ file_counter.py
@@ -1,27 +1,30 @@
 import os
 
-# Write the name of the directory here,
-# that needs to get sorted
-path = input(r'')
-os.chdir(path)
-# This will create a properly organized
-# list with all the filename that is
-# there in the directory
-list_ = os.listdir(path)
-# first folder to count files in
-AppleDouble = 0
-file0 = 0
-for file_ in list_:
-    # This excludes any file formats from MacOS or Apple's operating system
-    # This if statement is an AppleDouble Format
-    #       This format is to prevent loss of a file's metadata when it is moved
-    #       from the macOS system to other file systems
-    if file_[0:2] == "._":
-        AppleDouble += 1
-    elif file_[0] == ".":
-       continue
-    else:
-        file0 += 1
-print(f"In {path} there are "
-      f"\n\t{file0} files"
-      f"\n\t{AppleDouble} AppleDouble files")
+"""Write the name of the directory here, that needs to get sorted"""
+def list_directory_contents(input_path):
+    path = fr'{input_path}'
+    os.chdir(path)
+    # This will create a properly organized list with all the filename that is there in the directory
+    list_ = os.listdir(path)
+    # first folder to count files in
+    AppleDouble = 0
+    file0 = 0
+    for file_ in list_:
+        """ 
+        This excludes any file formats from MacOS or Apple's operating system
+         This if statement is an AppleDouble Format
+               This format is to prevent loss of a file's metadata when it is moved
+               from the macOS system to other file systems
+        """
+        if file_[0:2] == "._":
+            AppleDouble += 1
+        elif file_[0] == ".":
+           continue
+        else:
+            file0 += 1
+    print(f"In {path} there are "
+          f"\n\t{file0} files"
+          f"\n\t{AppleDouble} AppleDouble files")
+
+if __name__ == "__main__":
+    list_directory_contents(r"C:\Users\Audrey\OneDrive\Pictures\Vickys_Photos-backup\google takeout 8-6-25")
--- subfile_counter.py
+++ subfile_counter.py
@@ -1,7 +1,7 @@
 import os
+from pathlib import Path
 
-# Write the name of the directory here,
-# that needs to get sorted
+# Write the name of the directory here that needs to get sorted
 class Files:
     def __init__(self, path1):
         self.path1 = path1
@@ -24,40 +24,51 @@ class Files:
         for path in os.scandir(self.path1):
             if path.is_file():
                 self.count += 1
-            else:
+            elif path.is_dir():
                 self.foldercount += 1
+            else:
+                continue
         print(f'In file path {file0}, there are\n{self.count} files')
         print(f'{self.foldercount} folders')
 
     def subfile_num(self, subfile):
-        count1 = 0
-        foldercount1 = 0
+        filecount1 = 0
+        directorycount1 = 0
         AppleDouble = 0
+        SupplementalFiles = 0
         file0 = str(subfile)
         os.chdir(subfile)
         for path in os.scandir(subfile):
             if path.is_file():
                 pathtxt = str(path)
-                # indexed that way since the first ten spaces are occupied by:
-                # [<DirEntry ']
-                # [0123456789A]
+                p = Path(pathtxt)
+                """
+                indexed that way since the first ten spaces are occupied by:
+                [<DirEntry ']
+                [0123456789A]
+                """
                 if pathtxt[11:13] == "._":
                     AppleDouble += 1
                 elif pathtxt[11:20] == ".DS_Store":
                     print(f'\niOS file: {pathtxt[11:20]}')
                     continue
+                elif p.suffix == ".json'>":
+                    SupplementalFiles += 1
                 else:
-                    count1 += 1
+                    filecount1 += 1
+            elif path.is_dir():
+                directorycount1 += 1
             else:
-                foldercount1 += 1
-        print(f'In {file0}, there are'
-              f'\n\t{count1} files'
+                continue
+        print(f'In {file0}, '
+              f'\nthere are a total of {filecount1 + AppleDouble + SupplementalFiles + directorycount1} items'
+              f'\n\t{filecount1} files'
               f'\n\t{AppleDouble} AppleDouble files'
-              f'\n\t{foldercount1} folders')
+              f'\n\t{SupplementalFiles} JSON files'
+              f'\n\t{directorycount1} directories')
 
     def subfile_total(self, subfile):
         self.count1 = 0
-        file0 = str(subfile)
         os.chdir(subfile)
         for path in os.scandir(subfile):
             if path.is_file():
@@ -66,8 +77,10 @@ class Files:
         return self.count1
 
 if __name__ == "__main__":
-    print("Type in file path:")
-    f = Files(input(r''))
+    # print("Type in file path:")
+    # f = Files(input(r''))
+    path = r'C:\Users\Audrey\OneDrive\Pictures\Vickys_Photos-backup\google takeout 8-6-25'
+    f = Files(path)
 
     f.file_num()
     f.list_folders()
@@ -84,10 +97,14 @@ if __name__ == "__main__":
         elif folders[0] == ".":
             print(f"iOS file: {folders}")
             continue
+        elif folders[-5:] == ".json":
+            path = os.path.join(f.path1, folders)
+            print(f"{folders}")
         else:
             path = os.path.join(f.path1, folders)
-            #print(f"\n{path}")
             f.subfile_num(path)
+
+        print("\n")
 
 
 #C:\Users\audre\Desktop\Vicky_s_Photos
